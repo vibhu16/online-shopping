57bca5c6cf8957f31f9ebd03bf681cb4f0fd9fe4
vivek <ervivekdhasmana@gmail.com>
2017-08-23 10:12:57 +0530

    updated themes and footer

--- onlineshopping/src/main/java/com/vivek/onlineshopping/controller/PageController.java
+++ onlineshopping/src/main/java/com/vivek/onlineshopping/controller/PageController.java
@@ -1,7 +1,6 @@
 package com.vivek.onlineshopping.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -12,19 +11,28 @@ public class PageController {
 	public ModelAndView index() {
 
 		ModelAndView mv = new ModelAndView("page");
-		mv.addObject("greeting", "welcome to spring mvc");
+		mv.addObject("title", "Home");
+		mv.addObject("userClickHome", true);
 		return mv;
 
 	}
 
-	@RequestMapping(value = { "/test/{greeting}" })
-	public ModelAndView test(@PathVariable(value = "greeting") String greeting) {
-
-		if (greeting == null)
-			greeting = "hello there";
+	@RequestMapping(value = "/about")
+	public ModelAndView about() {
 
 		ModelAndView mv = new ModelAndView("page");
-		mv.addObject("greeting", greeting);
+		mv.addObject("title", "About Us");
+		mv.addObject("userClickAbout", true);
+		return mv;
+
+	}
+
+	@RequestMapping(value = "/contact")
+	public ModelAndView contact() {
+
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("title", "Contact Us");
+		mv.addObject("userClickContact", true);
 		return mv;
 
 	}
